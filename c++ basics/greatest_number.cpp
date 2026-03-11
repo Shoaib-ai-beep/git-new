8b22be6aaab721317a7a8dd0df7fce378b4386cc
Mohammad Shoaib <mds235193@gmail.com>
2026-03-11 13:46:10 +0530

    mus

--- c++ basics/greatest_number.cpp
+++ c++ basics/greatest_number.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-int a , b;
-cout<<"Enter the first number : ";
-cin>>a;
-cout<<"Enter the second number: "; 
-cin>>b;
-if(a>b)
+int main()
 {
-    cout<<" The first number is greater";
+    int a, b;
+    cout << "Enter the first number : ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    if (a > b)
+    {
+        cout << " The first number is greater";
+    }
+    if (a == b)
+    {
+        cout << "Both  numbers are equal";
+    }
+    else
+    {
+        cout << "Second number is greater";
+    }
+}
 
-}
- if(a==b)
-{
-    cout<<"Both  numbers are equal";
-}
-else{
-    cout<<"Second number is greater";
-}
-}
+// mus ke laika
